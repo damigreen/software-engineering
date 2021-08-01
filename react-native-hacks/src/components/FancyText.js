9352784b6d01f8d4984e0e4359371c0d3c9ab135
Damilola Faseun <damilola.faseun@gmail.com>
2021-08-01 12:10:54 +0100

    Fancy text component

---
+++ react-native-hacks/src/components/FancyText.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native'
+
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'grey',
+    fontSize: 14,
+  },
+  blueText: {
+    color: 'blue',
+  },
+  bigText: {
+    fontSize: 24,
+    fontWeight: '700'
+  },
+});
+
+
+const FancyText = ({ isBlue, isBig, children }) => {
+  const textStyles = [
+    styles.text,
+    isBlue && styles.blueText,
+    isBig && styles.bigText,
+  ];
+
+  return <Text style={textStyles}>{children}</Text>;
+};
+
+export default FancyText;
