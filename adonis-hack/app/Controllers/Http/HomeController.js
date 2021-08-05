5b516d0e47b723a279cf8b3f7eb22289e0717610
Damilola Faseun <damilola.faseun@gmail.com>
2021-08-05 15:32:11 +0100

    removed deleted files

--- adonis-hack/app/Controllers/Http/HomeController.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-class HomeController {
-  showHomepage ({ view }) {
-    return view.render('home');
-  }
-}
-
-module.exports = HomeController
--- adonis-hack/app/Models/Property.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-const Model = use('Model')
-
-class Property extends Model {
-}
-
-module.exports = Property
